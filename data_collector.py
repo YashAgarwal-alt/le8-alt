da54eb4c614da4e611bb21eceb4e1321e21ad4a2
YashAgarwal-alt <yashleo81@gmail.com>
2021-10-29 18:09:04 -0400

    final commit

--- data_collector.py
+++ data_collector.py
@@ -1,15 +1,17 @@
 import sys
 
-def data_collect(f):
+def data_collect(f, written_to):
     file = open(f, "r")
 
     instruction_storage = dict()
     data_storage = dict()
 
     for line in file.readlines():
-        temp = line.split(",");
+        temp = line.split(",")
         tracer = temp[0][:-3]
-        access_type = temp[1]
+        access_type = temp[1].strip()
+        # print(access_type)
+        # print(type(access_type))
         if access_type == "I":
             if tracer in instruction_storage:
                 instruction_storage[tracer] += 1
@@ -20,13 +22,20 @@ def data_collect(f):
                 data_storage[tracer] += 1
             else:
                 data_storage[tracer] = 1
-    
-    print("Instructions:")
-    for x in sorted(instruction_storage.items(), reverse=True):
-	    print(x[0] + "," + str(x[1]))
-    print("Data:")
-    for x in sorted(data_storage.items(), reverse=True):
-        print(x[0] + "," + str(x[1]))
+    file.close()
 
-if __name__ == "main":
-    data_collect(sys.argv[1])
+    print(data_storage)
+    print("-------------------------------")
+    print(instruction_storage)
+    file = open(written_to, "w")
+    file.write("Instructions:\n")
+    for x in sorted(instruction_storage.items(), reverse=True, key=lambda entry: entry[1]):
+        file.write(x[0] + "," + str(x[1]) + "\n")
+    file.write("\nData:\n")
+    for x in sorted(data_storage.items(), reverse=True, key=lambda entry: entry[1]):
+        file.write(x[0] + "," + str(x[1]) + "\n")
+    file.close()
+
+
+data_collect("tr-heaploop.ref", "tr-heaploop-data.txt")
+data_collect("tr-matmul.ref", "tr-matmul-data.txt")
